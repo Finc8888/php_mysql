d840308a1aecb761fae0e0f1498118fe1477055a
Finc8888 <con8@mail.ru>
2018-06-17 20:53:40 +0300

    Adding query and constant

---
+++ app_config.php
@@ -0,0 +1,6 @@
+<?php
+define("DATABASE_HOST","localhost");
+define("DATABASE_USERNAME","root");
+define("DATABASE_PASSWORD","m31415926");
+define("DATABASE_NAME","sys");
+?>
--- connect.php
+++ connect.php
@@ -1,14 +1,43 @@
-<!doctype html>
-<html>
-  <head>
-    <title>MySql test</title>
-    <meta charset="utf-8">
-  </head>
-  <body style="color: red">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>MySql test form</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <div class="red">
     <?php
-    mysql_connect("localhost","root","m31415926")
-      or die("<h3>Ошибка подключения к базе данных ". mysql_error.
-              "</h3>");
-    echo "<h2> Вы подключились к MySql!</h2>";
+    require 'app_config.php';
+      mysql_connect(DATABASE_HOST,DATABASE_USERNAME,DATABASE_PASSWORD)
+        or die("<h3>Ошибка подключения к базе данных ". mysql_error().
+                "</h3>");
+      echo "<h2> Вы подключились к MySql!</h2>";
+
+      mysql_select_db(DATABASE_NAME)
+        or die("<h3>Ошибка при выборе базы данных: ". DATABASE_NAME." ".
+         mysql_error()."</h3>");
+      echo "<h2> Вы подключились к MySql с использованием БД: ". DATABASE_NAME.
+      "</h2>";
     ?>
-  </body>
+  </div>
+  <div class="green">
+    <?php
+    require 'app_config.php';
+    $query = mysql_query("SHOW TABLES;");
+      if (!$query) {
+        die("<h3>Ошибка при выводе перечня таблиц: ". mysql_error().
+                "</h3>");
+      }
+      echo "<h3> Таблицы, имеющиеся в базе данных:</h3>";
+      echo "<ul>";
+      while($row = mysql_fetch_row($query)){
+        echo "<li>Таблица:{$row[0]}</li>";
+      }
+      echo "</ul>";
+    ?>
+  </div>
+
+</body>
